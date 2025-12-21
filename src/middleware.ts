52cb93498c9542fb0c276317c9869880599f35dc
Andrewdddobusiness <andrewdddobusiness@gmail.com>
2025-12-21 13:40:49 +1100

    feat: skip `api` and `trpc` routes in middleware matcher.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!api|trpc|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
 };
